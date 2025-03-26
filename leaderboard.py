a0598cac3d6015dc093cee4fd72d4ff2f483dae3
hexagon prime <itzdrkek2@gmail.com>
2025-03-26 21:45:43 +0700

    Add files via upload

--- leaderboard.py
+++ leaderboard.py
@@ -1,2 +1,20 @@
-name <--- имя игрока
-score <--- очки игрока
+import csv
+import time
+
+score=input("score")
+username=input("name")
+
+with open ("protleader.txt", "a", newline='') as file:
+    fields=['score', 'name']
+    writer=csv.DictWriter(file, fieldnames=fields)
+    writer.writerow({'score' : score, 'name' : username})
+
+with open ("protleader.txt", "r") as file:
+    sortlist=[]
+    reader=csv.reader(file)
+    for i in reader:
+        sortlist.append(i)
+
+for i in range(len(sortlist)):
+    if i != 0:
+        sortlist[i][0]=int(sortlist[i][int(0)])
