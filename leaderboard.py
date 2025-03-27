dbdf387ef01a3cd62f7686af5537106c0d6cada0
MECHMAN3501 <vova.sol2011@yandex.ru>
2025-03-27 22:35:28 +0700

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -1,27 +1,33 @@
 # код Савелия Жердеева
+# приведение кода к PEP8 Савелия Жердеева
 import csv
 import time
 def SetNewScore(name: str, score: int):
-
-    with open ('leaderboard.csv', 'a', newline='', encoding='utf-16') as file:
-        fields=['score', 'name']
-        writer=csv.DictWriter(file, fieldnames=fields)
+    '''
+    Функция которая добавляет новый рекорд
+    '''
+    with open ('leaderboard.csv', 'a', newline = '', encoding = 'utf-16') as file:
+        fields = ['score', 'name']
+        writer = csv.DictWriter(file, fieldnames = fields)
         writer.writerow({'score' : score, 'name' : name})
         file.close()
-    with open ('leaderboard.csv', 'r', encoding='utf-16') as file:
-        reader=csv.reader(file)
-        reader = sorted(reader, key=lambda row: int(row[0]), reverse=True)
+    with open ('leaderboard.csv', 'r', encoding = 'utf-16') as file:
+        reader = csv.reader(file)
+        reader = sorted(reader, key = lambda row: int(row[0]), reverse = True)
         file.close()
         
-    with open ('leaderboard.csv', 'w', newline='', encoding='utf-16') as file:
-        csvwriter=csv.writer(file)
+    with open ('leaderboard.csv', 'w', newline = '', encoding = 'utf-16') as file:
+        csvwriter = csv.writer(file)
         for i in reader:
             csvwriter.writerow(i)
         file.close()
 def GetScores():
-    with open ('leaderboard.csv', 'r', encoding='utf-16') as file:
-        scorelist=[]
-        reader=csv.reader(file)
+    '''
+    Функция которая возвращает все рекорды в формате [рекорд,имя]
+    '''
+    with open ('leaderboard.csv', 'r', encoding = 'utf-16') as file:
+        scorelist = []
+        reader = csv.reader(file)
         for i in reader:
             scorelist.append(i)
         return scorelist
